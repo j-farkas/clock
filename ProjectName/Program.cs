a9c4f2a2b1124283b20015f25a41c03c6307316c
Jared Farkas and Lindsey Baker <lindsey@>
2019-04-23 10:04:42 -0700

    Add function to return pingpong array

---
+++ ProjectName/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace ProjectName{
+
+  public class Program
+  {
+    public static void Main()
+    {
+      PingPong numberInput = new PingPong(35);
+
+
+    numberInput.DisplayString();
+    }
+  }
+}
